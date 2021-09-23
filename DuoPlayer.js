0c9890787922da09b220e6e95b137044baeb5ebf
unknown <nilayd@nycstudents.net>
2021-09-22 22:54:37 -0400

    7

--- DuoPlayer.js
+++ DuoPlayer.js
@@ -13,7 +13,7 @@ let selectedCol;
 let pieces = []
 for (let i=0;i<42;i++){
     pieces[i] = document.createElement('div')
-    pieces[i].style = "--topPos: 0px; --col: 1; --row: 7"
+    pieces[i].style = "--topPos: 0px; --endCol: 1; --row: 7; --startCol: 1; --dropCol: 1;"
     pieces[i].classList.add('circle')
     pieces[i].classList.add('follow')
 }
@@ -21,7 +21,10 @@ for (let i=0;i<cols.length;i++){
     cols[i].style.gridColumn = i+1;
     cols[i].addEventListener('mouseover', event=>{
         container.appendChild(pieces[count])
-        pieces[count].style.setProperty('--col', ''+(i+1))
+        pieces[count].style.setProperty('--endCol', returnColVal("" + (i+1)) + 'vw')
+        pieces[count].addEventListener('animationend',()=>{
+            pieces[count].style.setProperty('--startCol', returnColVal("" + (i+1)) + 'vw')
+        })
     })
 }
 container.addEventListener('mouseleave', ()=>{
@@ -38,3 +41,21 @@ container.addEventListener('click', ()=>{
     
    
 })
+
+
+
+
+
+
+
+function returnColVal(col){
+    switch (col){
+        case "1": return 0; break;
+        case "2": return 1*(55/7); break;
+        case "3": return 2*(55/7); break;
+        case "4": return 3*(55/7); break;
+        case "5": return 4*(55/7); break;
+        case "6": return 5*(55/7); break;
+        default: return 6*(55/7);
+    }
+}
